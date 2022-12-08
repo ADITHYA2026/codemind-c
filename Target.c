557806a9fa6610c83a6f8e5c8e23415bf2083cf5
ADITHYA2026 <119736431+ADITHYA2026@users.noreply.github.com>
2022-12-08 14:49:44 +0530

    #include<stdio.h> int pr(int a,int b,int c,int d) {     if (a>=10 && b>=10 && c>=10 &&d>=10)     return 1; } int main () {     int a,b,c,d,t;     scanf("%d %d %d %d",&a,&b,&c,&d);     t=pr(a,b,c,d);     if (t==1)     printf("YES");     else     printf("NO"); }

--- Target.c
+++ Target.c
@@ -1,14 +1,16 @@
 #include<stdio.h>
-#include<math.h>
-void pro(int a,int b,int c,int d)
+int pr(int a,int b,int c,int d)
 {
-    if (a>=10 && b>=10 && c>=10 && d>=10)
-    printf("YES");
-    else
-    printf("NO");
+    if (a>=10 && b>=10 && c>=10 &&d>=10)
+    return 1;
 }
-int main () {
-    int a,b,c,d;
+int main () 
+{
+    int a,b,c,d,t;
     scanf("%d %d %d %d",&a,&b,&c,&d);
-    pro(a,b,c,d);
+    t=pr(a,b,c,d);
+    if (t==1)
+    printf("YES");
+    else 
+    printf("NO");
 }
