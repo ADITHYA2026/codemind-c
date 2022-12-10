e755a7db02f2ad23f9f9ce98ef23129a2688b432
ADITHYA2026 <119736431+ADITHYA2026@users.noreply.github.com>
2022-12-10 14:52:10 +0530

    #include<stdio.h> int main () {     int n,i,flag=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     scanf("%d",&a[i]);     for(i=0;i<n;i++)     {       int k;       scanf("%d",&k);       if(a[i]==k)       {           flag=1;           break;       }     }     if(flag==1)     printf("True");     else     printf("False"); }

---
+++ element_search_in_an_array.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main ()
+{
+    int n,i,flag=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    for(i=0;i<n;i++)
+    {
+      int k;
+      scanf("%d",&k);
+      if(a[i]==k)
+      {
+          flag=1;
+          break;
+      }
+    }
+    if(flag==1)
+    printf("True");
+    else
+    printf("False");
+}
