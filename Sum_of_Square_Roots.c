21f2dad02610c2c5906e31fb474964b0ebd505f6
ADITHYA2026 <119736431+ADITHYA2026@users.noreply.github.com>
2022-12-22 19:07:10 +0530

    #include<stdio.h> #include<math.h> int main () {     int a,b,i;     float c,s=0;     scanf("%d %d",&a,&b);     for(i=a;i<=b;i++)     {         c=sqrt(i);         s=s+c;     }     printf("%.2f",s); }

---
+++ Sum_of_Square_Roots.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+#include<math.h>
+int main ()
+{
+    int a,b,i;
+    float c,s=0;
+    scanf("%d %d",&a,&b);
+    for(i=a;i<=b;i++)
+    {
+        c=sqrt(i);
+        s=s+c;
+    }
+    printf("%.2f",s);
+}
