a290b1f9099f3dc6bc086dbab88940c97167a7ea
ADITHYA2026 <119736431+ADITHYA2026@users.noreply.github.com>
2022-12-24 20:42:43 +0530

    #include<stdio.h> int main() {     int n,r;     scanf("%d",&n);     while(n>0)     {         r=n%10;         n=n/10;         if(r%2 && n%2)         {         printf("Odd");         return 0;         }         else if(r%2==0 && n%2==0)         {         printf("Even");         return 0;         }         else         {         printf("Mixed");         return 0;         }     } }

---
+++ Even_Odd_Mixed.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n,r;
+    scanf("%d",&n);
+    while(n>0)
+    {
+        r=n%10;
+        n=n/10;
+        if(r%2 && n%2)
+        {
+        printf("Odd");
+        return 0;
+        }
+        else if(r%2==0 && n%2==0)
+        {
+        printf("Even");
+        return 0;
+        }
+        else
+        {
+        printf("Mixed");
+        return 0;
+        }
+    }
+}
