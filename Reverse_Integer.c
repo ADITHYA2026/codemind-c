60098928a4f2c8636536658be29bc5ea6991d17b
ADITHYA2026 <119736431+ADITHYA2026@users.noreply.github.com>
2022-12-27 18:28:52 +0530

    #include<stdio.h> int main() {     int n,s=0,r;     scanf("%d",&n);     while(n!=0)     {         r=n%10;         n=n/10;         if(r==0)         {             continue;         }         s=s*10+r;     }     printf("%d",s);     return 0; }

---
+++ Reverse_Integer.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,s=0,r;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        r=n%10;
+        n=n/10;
+        if(r==0)
+        {
+            continue;
+        }
+        s=s*10+r;
+    }
+    printf("%d",s);
+    return 0;
+}
