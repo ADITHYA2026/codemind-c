a4983b884c5259fb15078ba0c17c03f1113bf58c
ADITHYA2026 <119736431+ADITHYA2026@users.noreply.github.com>
2022-12-30 17:01:03 +0530

    #include<stdio.h> int main () {         int n,i,j;         scanf("%d",&n);         for(i=1;i<=n;i++)         {             for(j=1;j<=i;j++)             {                 printf("%d",j);             }
               printf("
    ");
            }
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 int main ()
 {
-    int n,i,j;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
-    {
-        for(j=n;j>=1;j--)
+        int n,i,j;
+        scanf("%d",&n);
+        for(i=1;i<=n;i++)
         {
-            printf("%d ",j);
-        }
-        printf("
+            for(j=1;j<=i;j++)
+            {
+                printf("%d",j);
+            }
+     
+           printf("
 ");
-    }
+        }
 }
