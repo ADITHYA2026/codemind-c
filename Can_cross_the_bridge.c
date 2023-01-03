603c7073c90754319a74010da291bb9f0e907415
ADITHYA2026 <119736431+ADITHYA2026@users.noreply.github.com>
2023-01-03 18:22:53 +0530

    #include<stdio.h> #include<math.h> int mangoes(int x,int y,int z) {     int c;     c=(z-y)/x;
    }
    int main ()
    {
        int x,y,z,k;
        scanf("%d %d %d",&x,&y,&z);
        k=mangoes(x,y,z);
            printf("%d",k);
    }

--- Can_cross_the_bridge.c
+++ Can_cross_the_bridge.c
@@ -4,12 +4,12 @@ int mangoes(int x,int y,int z)
 {
     int c;
     c=(z-y)/x;
-     return c;
+
 }
 int main ()
 {
     int x,y,z,k;
     scanf("%d %d %d",&x,&y,&z);
     k=mangoes(x,y,z);
-    printf("%d",k);
+        printf("%d",k);
 }
