191c116f42a864127d8a4abe7bf47b9282c2657c
ADITHYA2026 <119736431+ADITHYA2026@users.noreply.github.com>
2023-01-03 18:05:51 +0530

    #include<stdio.h> #include<math.h> float area(int a,int b,int c) {     float d;     d=(a+b)*c*0.5;     return d; } int main () {     int a,b,c;     float k;     scanf("%d %d %d",&a,&b,&c);     k=area(a,b,c);     printf("%0.4f",k); }

--- Program_to_calculate_the_area_of_Trapezium.c
+++ Program_to_calculate_the_area_of_Trapezium.c
@@ -1,14 +1,16 @@
 #include<stdio.h>
 #include<math.h>
-void area(int a,int b,int c)
+float area(int a,int b,int c)
 {
     float d;
     d=(a+b)*c*0.5;
-    printf("%0.4f",d);
+    return d;
 }
 int main ()
 {
     int a,b,c;
+    float k;
     scanf("%d %d %d",&a,&b,&c);
-    area(a,b,c);
+    k=area(a,b,c);
+    printf("%0.4f",k);
 }
