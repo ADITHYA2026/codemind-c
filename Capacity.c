a424ea19d2cc2b70e9e3850731ec7ec940d32380
ADITHYA2026 <119736431+ADITHYA2026@users.noreply.github.com>
2023-01-07 20:27:27 +0530

    #include<stdio.h> int main () {  int a,b,c,d;  scanf("%d %d %d",&a,&b,&c);  d=a*b*c;  printf("%d KB",d); }

--- Capacity.c
+++ Capacity.c
@@ -1,17 +1,7 @@
 #include<stdio.h>
-int kb(int s,int t,int b)
-{
-    
-    int c,k;
-    c=1024*s*t*b;
-    k=s*t*b;
-    return k;
-}
 int main () {
-   
-    int s,t,b,tk;
-    scanf("%d %d %d",&s,&t,&b);
-    tk=kb(s,t,b);
-    printf("%d KB",tk);
-    
+ int a,b,c,d;
+ scanf("%d %d %d",&a,&b,&c);
+ d=a*b*c;
+ printf("%d KB",d);
 }
