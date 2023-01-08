b097994b085dada6d6893016979190842d2caeee
ADITHYA2026 <119736431+ADITHYA2026@users.noreply.github.com>
2023-01-08 11:11:16 +0530

    #include<stdio.h> int main () {     int i,n;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         if(i%2==1)         printf("%d ",i);     } }

--- Odd Natural_Numbers.c
+++ Odd Natural_Numbers.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
-int main () {
-    int a,i,n;
+int main ()
+{
+    int i,n;
     scanf("%d",&n);
     for(i=1;i<=n;i++)
-    if(i%2!=0)
-    printf("%d ",i);
+    {
+        if(i%2==1)
+        printf("%d ",i);
+    }
 }
